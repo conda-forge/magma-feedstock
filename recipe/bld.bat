0e78be40425a1509e814a9ff6af4a27ac9796522
Daniel Ching <carterbox@users.noreply.github.com>
2023-02-22 16:08:21 -0600

    Add TMP directory for bash on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,12 @@
 @echo on
 
+md .\tmp
+set "TMPDIR=.\tmp"
+if errorlevel 1 exit /b 1
+
 :: This step is required when building from raw source archive
 make generate --jobs %CPU_COUNT%
+if errorlevel 1 exit /b 1
 
 :: Duplicate lists because of https://bitbucket.org/icl/magma/pull-requests/32
 set "CUDA_ARCH_LIST=sm_35,sm_50,sm_60,sm_61,sm_70,sm_75"
