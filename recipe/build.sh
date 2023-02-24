3f4b78b74a4cb14c3c65ef73d9037cb32e6fb4cc
Daniel Ching <carterbox@users.noreply.github.com>
2023-02-23 18:07:39 -0600

    Redistribute CUDA archs

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,23 +3,20 @@ set -exv
 # This step is required when building from raw source archive
 make generate --jobs ${CPU_COUNT}
 
+# https://en.wikipedia.org/wiki/CUDA#GPUs_supported
+# The logic for the targeted archs is as follows:
+# 1. We build the minimum supported arch with both real and virtual to maximize
+# compatability
+# 2. Skip all deprecated archs after min supported
+# 3. Start cutting minor versions in order to fit into 6 hour build time
 # Duplicate lists because of https://bitbucket.org/icl/magma/pull-requests/32
-export CUDA_ARCH_LIST="sm_35,sm_50,sm_60,sm_61,sm_70,sm_75"
-export CUDAARCHS="35;50;60;61;70;75"
+export CUDA_ARCH_LIST="sm_35,sm_60,sm_61,sm_70,sm_75,sm_80,sm_86"
+export CUDAARCHS="35-virtual;60-virtual;61-real;70-virtual;75-real;80-virtual;86"
 
-if awk "BEGIN {exit !($cuda_compiler_version >= 11.0)}"; then
-  CUDA_ARCH_LIST="$CUDA_ARCH_LIST,sm_80"
-  CUDAARCHS="$CUDAARCHS;80"
-fi
-if awk "BEGIN {exit !($cuda_compiler_version >= 11.1)}"; then
-  CUDA_ARCH_LIST="$CUDA_ARCH_LIST,sm_86"
-  CUDAARCHS="$CUDAARCHS;86"
-fi
-
-# Only build recent archs for Power and Arm to reduce build time
+# Only build the lowest non-deprecated arch to minimize build time
 if [[ "$target_platform" == "linux-ppc64le" || "$target_platform" == "linux-aarch64" ]]; then
-  export CUDA_ARCH_LIST="sm_70,sm_80"
-  export CUDAARCHS="70-virtual;80-virtual"
+  export CUDA_ARCH_LIST="sm_60"
+  export CUDAARCHS="60"
 fi
 
 # Remove CXX standard flags added by conda-forge. std=c++11 is required to
