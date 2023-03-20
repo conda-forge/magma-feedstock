b0b9e9d5edb0985bce1eb4b0408594c58552e141
Daniel Ching <carterbox@users.noreply.github.com>
2023-03-20 18:06:32 -0500

    Do not install make files

--- recipe/install_headers.sh
+++ recipe/install_headers.sh
@@ -1,5 +1,5 @@
 set -exv
 
-cp -pr ./include/ $PREFIX
+cp -pr ./include/*.h $PREFIX
 install -D ./build/include/magma_config.h $PREFIX/include/magma_config.h
 install -D ./build/lib/pkgconfig/magma.pc $PREFIX/lib/pkgconfig/magma.pc
