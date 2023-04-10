411fdc1e0cbc91f867d3384c56aca6c8caf30d5e
Daniel Ching <carterbox@users.noreply.github.com>
2023-04-10 10:56:43 -0500

    BUG: Install headers in $PREFX/include
    Closes conda-forge/magma-feedstock#28

--- recipe/install_headers.bat
+++ recipe/install_headers.bat
@@ -1,6 +1,8 @@
 @echo on
 
-xcopy /s /k /y .\include\*.h %LIBRARY_PREFIX%
+md %LIBRARY_PREFIX%\include
+xcopy /s /k /y .\include\*.h %LIBRARY_PREFIX%\include
+xcopy /s /k /y .\sparse\include\*.h %LIBRARY_PREFIX%\include
 cp .\build\include\magma_config.h %LIBRARY_PREFIX%\include\magma_config.h
 md  %LIBRARY_PREFIX%\lib\pkgconfig
 cp .\build\lib\pkgconfig\magma.pc %LIBRARY_PREFIX%\lib\pkgconfig\magma.pc
